174a2b91add3e5bb6b3086dc789cff04f0f8b921
Aryan Agarwal <aryankush025@gmail.com>
2023-01-04 00:17:52 +0530

    Login & SignUp layout added

--- src/routes/(unauthenticated)/+layout.svelte
+++ src/routes/(unauthenticated)/+layout.svelte
@@ -1,13 +1,27 @@
-<div>
-	<main>
-		<slot />
-	</main>
+<div class="min-h-screen flex">
+	<div class="py-6 px-4 flex flex-col justify-between min-h-screen w-full sm:w-2/4">
+		<header>
+			<img alt="glue_logo" src="src/lib/assets/images/glue.png" class="h-14 w-14 mx-auto" />
+		</header>
 
-	<footer>
-		<p>
-			<a href="/terms">Terms of Service</a>
-			|
-			<a href="/policy">Privacy Policy.</a>
-		</p>
-	</footer>
+		<main>
+			<slot />
+		</main>
+
+		<footer>
+			<p class="text-sm text-center text-gray-500">
+				<a href="/terms">Terms of Service</a>
+				|
+				<a href="/policy">Privacy Policy</a>
+			</p>
+		</footer>
+	</div>
+
+	<div class="w-2/4 hidden h-screen flex-1 sm:block">
+		<img
+			alt="side_image"
+			src="https://images.unsplash.com/photo-1525498128493-380d1990a112?auto=format&fit=crop&w=500&q=60"
+			class="w-full h-full object-cover"
+		/>
+	</div>
 </div>
